a93a8126327b759c14678809899cc7dc77f6f799
loren <kerlo392@student.otago.ac.nz>
2018-02-25 11:03:45 +1300

    Extended sql.py to retrieve items from database

--- read_temperatures.py
+++ read_temperatures.py
@@ -4,8 +4,12 @@ import sql
 import time 
 
 ser = serial.Serial('/dev/ttyUSB0', 9600)
+
 # Expected format to extract from the serial port
 expected = r"temperature;[0-9];[0-9,.,-]{2,8}" 
+
+print sql.get_temperatures(1, 100)
+
 
 while ser.isOpen():
     line = ser.readline()
--- sql.py
+++ sql.py
@@ -1,4 +1,6 @@
 import sqlite3
+import time
+import numpy as np 
 
 db = 'brew_db.db'
 
@@ -21,4 +23,35 @@ def write_temperature(probe_id, temp, timestamp):
     conn.commit()
 
 
+def get_temperatures(ID):
+    conn, c = get_db(db)
+    try:
+        c.execute('''SELECT temperature, time FROM temperatures
+                      WHERE probe=?''', (ID,))
+        ret = np.asarray(c.fetchall())
+    except:
+        print 'Problem retreiving temperatures'
+        pass
+
+    temps = list(ret[:, 0])
+    times = list(ret[:, 1])
+
+    #print temps, times
+    return {'probe_id': ID, 'temperaures':temps, 'timestamps':times}
+
+    
+def get_temperatures_interval(ID, time_interval):
+    conn, c = get_db(db)
+    try:
+        c.execute('''SELECT temperature, time FROM temperatures
+                      WHERE probe=? AND (? - time < ?)''', (ID,time.time(), time_interval))
+        ret = c.fetchall()   
+    except:
+        print 'Problem retreiving temperatures'
+        pass
+    return ret
+
 setup_db(db)
+
+
+print get_temperatures(1)
